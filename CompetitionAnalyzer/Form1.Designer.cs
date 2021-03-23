1b3edfae41eb31f4469d3b2ca3f8120fa04c7918
canozaslan <a.canozaslan@gmail.com>
2021-03-23 12:23:21 +0300

    first commit

---
+++ CompetitionAnalyzer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace CompetitionAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dg_products = new System.Windows.Forms.DataGridView();
+            this.label_product_name = new System.Windows.Forms.Label();
+            this.tb_url = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_products)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(351, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verileri Getir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(102, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 24);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(567, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 22);
+            this.label2.TabIndex = 3;
+            // 
+            // dg_products
+            // 
+            this.dg_products.AllowUserToAddRows = false;
+            this.dg_products.AllowUserToResizeColumns = false;
+            this.dg_products.AllowUserToResizeRows = false;
+            this.dg_products.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dg_products.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_products.Location = new System.Drawing.Point(49, 104);
+            this.dg_products.Name = "dg_products";
+            this.dg_products.Size = new System.Drawing.Size(728, 198);
+            this.dg_products.TabIndex = 5;
+            // 
+            // label_product_name
+            // 
+            this.label_product_name.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_product_name.Location = new System.Drawing.Point(101, 31);
+            this.label_product_name.Name = "label_product_name";
+            this.label_product_name.Size = new System.Drawing.Size(622, 53);
+            this.label_product_name.TabIndex = 6;
+            // 
+            // tb_url
+            // 
+            this.tb_url.Location = new System.Drawing.Point(64, 328);
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(699, 20);
+            this.tb_url.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 466);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.label_product_name);
+            this.Controls.Add(this.dg_products);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg_products)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dg_products;
+        private System.Windows.Forms.Label label_product_name;
+        private System.Windows.Forms.TextBox tb_url;
+    }
+}
+
---
+++ CompetitionAnalyzer/Form1.cs
@@ -0,0 +1,180 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompetitionAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Uri url = new Uri("https://www.akakce.com/kahvalti-takimi/en-ucuz-bambum-b2666-bouquet-22-parca-2-kisilik-fiyati,37886773.html");
+            //Uri url = new Uri("https://www.akakce.com/ekmek-kutusu-sepeti/en-ucuz-bambum-solly-19-cm-ekmek-kutusu-fiyati,243110060.html");
+            dt.Rows.Clear();
+            if (tb_url.Text.Trim().Equals("")) return;
+            Uri url = new Uri(tb_url.Text);
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            client.Headers.Add("Accept: text/html, application/xhtml+xml, */*");
+            client.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
+
+            string html = client.DownloadString(url);
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(html);
+
+            var head = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/div/div[1]/h1")[0];
+            label_product_name.Text = head.InnerHtml;
+
+            var cal = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/p/i")[0];
+            string numb = cal.InnerHtml;
+            string a = string.Empty;
+            int number;
+            for (int i = 0; i < numb.Length; i++)
+            {
+                if (char.IsDigit(numb[i]))
+                {
+                    a += numb[i];
+                }
+            }
+            number = Convert.ToInt32(a);
+            if (number > 10) number = 10;
+            if (number > 0)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    var s = string.Empty;
+                    var p = string.Empty;
+
+                   
+                
+                    if (document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[3]") != null)
+                    {
+                        var cal1 = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[3]/ul/li[{i}]/a/span[1]/span")[0];
+                        s = cal1.InnerHtml;
+                    }
+                    else if (document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[2]")[0] != null)
+                    {
+                        var cal1 = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[2]/ul/li[{i}]/a/span[1]/span")[0];
+                        s = cal1.InnerHtml;
+                    }
+
+                    var n = 0;
+                    for (int j = 0; j < s.Length; j++)
+                    {
+
+
+                        if (char.IsDigit(s[j]))
+                        {
+
+                            p += s[j];
+                        }
+                        else
+                        {
+                            if (n == 0) p += ",";
+
+                            if (j == s.Length - 1) p += " TL";
+                            n++;
+                        }
+                    }
+                    if (p.Equals(", TL"))
+                    {
+                        p = string.Empty;
+                        if (document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[3]") != null)
+                        {
+                            var cal2 = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[3]/ul/li[{i}]/a/span[1]/span[2]")[0];
+                            s = cal2.InnerHtml;
+                        }
+                        else if (document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[2]")[0] != null)
+                        {
+                            var cal2 = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[2]/ul/li[{i}]/a/span[1]/span[2]")[0];
+                            s = cal2.InnerHtml;
+                        }
+                        
+
+                        var k = 0;
+                        for (int j = 0; j < s.Length; j++)
+                        {
+
+
+                            if (char.IsDigit(s[j]))
+                            {
+
+                                p += s[j];
+                            }
+                            else
+                            {
+                                if (k == 0) p += ",";
+
+                                if (j == s.Length - 1) p += " TL";
+                                k++;
+                            }
+                        }
+                    }
+
+                    string st = string.Empty;
+                    if (document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[3]") != null)
+                    {
+                        var cal3 = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[3]/ul/li[{i}]/a/span[5]/span")[0];
+                        st = cal3.InnerHtml;
+                    }
+                    else if(document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[2]")[0] != null)
+                    {
+                        var cal3 = document.DocumentNode.SelectNodes($"/html/body/main/div[1]/section/div[2]/ul/li[{i}]/a/span[5]/span")[0];
+                        st = cal3.InnerHtml;
+                    }
+                    
+                   
+
+                    if (st.Contains("<img"))
+                    {
+                        var q = st.Length;
+                        var stIndex = st.IndexOf("<span>") + 6;
+                        var fnIndex = st.Substring(stIndex).IndexOf("</span>");
+                        string company = st.Substring(stIndex, fnIndex);
+                        var lastIndex = st.LastIndexOf("</span>") + 7;
+                        company += st.Substring(lastIndex);
+                        dt.Rows.Add(company, p);
+                    }
+                    else if (!st.Contains("<img") && st.Contains("<span"))
+                    {
+                        var stIndex = st.IndexOf("<span");
+                        string company = st.Substring(0, stIndex);
+                        dt.Rows.Add(company, p);
+                    }
+                    else
+                    {
+                        var company = st;
+                        dt.Rows.Add(company, p);
+                    }
+  
+                    dg_products.DataSource = dt;
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dt.Columns.Add("Satıcı Firma");
+            dt.Columns.Add("Fiyat");
+            dg_products.DataSource = dt;
+
+        }
+    }
+}
